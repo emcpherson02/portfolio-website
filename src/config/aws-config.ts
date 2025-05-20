b5eb407fad5e1bc86054345ab93e01894390886c
Elliott McPherson <elliott.mcpherson985@gmail.com>
2025-05-20 12:18:49 +0100

    aws config.

--- src/config/aws-config.ts
+++ src/config/aws-config.ts
@@ -1,4 +1,3 @@
 export const awsConfig = {
-    imageBaseUrl: 'https://d3sc4ffh0e7l94.cloudfront.net',
-    documentsBaseUrl: 'https://d3sc4ffh0e7l94.cloudfront.net/documents'
+    imageBaseUrl: 'https://d3sc4ffh0e7l94.cloudfront.net'
 };
